7159542fe075468e2393b7c86dfceaad6e2c4341
Akiko Takano <akiko.pusu@gmail.com>
2019-08-18 10:30:39 +0900

    Add ecr lifecycle policy.

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,25 @@
 resource "aws_ecr_repository" "redmine_banner" {
   name = "redmine_banner"
 }
+
+resource "aws_ecr_lifecycle_policy" "redmine_banner_policy" {
+  policy = jsonencode(
+    {
+      rules = [
+        {
+          action = {
+            type = "expire"
+          }
+          description  = "最新の1つを残してイメージを削除する"
+          rulePriority = 1
+          selection = {
+            countNumber = 1
+            countType   = "imageCountMoreThan"
+            tagStatus   = "any"
+          }
+        },
+      ]
+    }
+  )
+  repository = "${aws_ecr_repository.redmine_banner.name}"
+}
